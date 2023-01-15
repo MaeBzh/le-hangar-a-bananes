5e5020bf9c3362d63b583ec0834ab9ff701f8185
Tijawk <julien.moulinpicaud@proton.me>
2023-01-16 00:52:40 +0100

    generate .env.production during github actions build

--- .vitepress/config/scripts/google_analytics.ts
+++ .vitepress/config/scripts/google_analytics.ts
@@ -1,23 +1,22 @@
-
+import { loadEnv } from "vite";
 import { HeadConfig } from "vitepress";
 
-const googleAnalyticsScripts = ({VITE_GOOGLE_ANALYTICS_ID}: Record<string, string>): HeadConfig[] => {
-    return VITE_GOOGLE_ANALYTICS_ID
-      ? [
-          [
-            "script",
-            {
-              async: "",
-              src: `https://www.googletagmanager.com/gtag/js?id=${VITE_GOOGLE_ANALYTICS_ID}`,
-            },
-          ],
-          [
-            "script",
-            {},
-            `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${VITE_GOOGLE_ANALYTICS_ID}');`,
-          ],
-        ]
-      : [];
-  };
+const { VITE_GOOGLE_ANALYTICS_ID } = loadEnv("production", process.cwd());
+const googleAnalyticsScripts: HeadConfig[] = VITE_GOOGLE_ANALYTICS_ID
+  ? [
+      [
+        "script",
+        {
+          async: "",
+          src: `https://www.googletagmanager.com/gtag/js?id=${VITE_GOOGLE_ANALYTICS_ID}`,
+        },
+      ],
+      [
+        "script",
+        {},
+        `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${VITE_GOOGLE_ANALYTICS_ID}');`,
+      ],
+    ]
+  : [];
 
-  export default googleAnalyticsScripts;
+export default googleAnalyticsScripts;
